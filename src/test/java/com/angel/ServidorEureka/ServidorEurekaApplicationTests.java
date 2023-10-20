0ae472d753e31d39c22e9d1aaa062df7fbc34b96
Ángel Iván Martínez <mgangelivan@gmail.com>
2023-10-20 16:40:48 -0600

    Servidor de Eureka con springboot crado

---
+++ src/test/java/com/angel/ServidorEureka/ServidorEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel.ServidorEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
